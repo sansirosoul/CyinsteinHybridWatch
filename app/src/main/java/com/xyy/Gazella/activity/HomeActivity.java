940050fda1fc7f4a21fcd0a45f24a8e9efe99471
sayid <13113639501@163.com>
2016-10-11 00:20:22 +0800

    Initial commit

--- app/src/main/java/com/xyy/Gazella/activity/HomeActivity.java
+++ app/src/main/java/com/xyy/Gazella/activity/HomeActivity.java
@@ -7,17 +7,10 @@ import com.ysp.newband.BaseActivity;
 import com.ysp.smartwatch.R;
 
 public class HomeActivity  extends BaseActivity{
-
-	private  String a ;
-	private  boolean b=false;
 	
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 		setContentView(R.layout.home_activity);
-
-		a ="fdfdsfsafsa";
-		b =true;
-		b=false;
 	}
 }
