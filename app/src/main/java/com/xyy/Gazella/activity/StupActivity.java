4ba2c8c4f1aa14b68d4d9d6f54bdc0f78bec6e22
sayid0927 <13113639501@163.com>
2016-10-11 17:43:32 +0800

    Initial commit

--- app/src/main/java/com/xyy/Gazella/activity/StupActivity.java
+++ app/src/main/java/com/xyy/Gazella/activity/StupActivity.java
@@ -32,36 +32,15 @@ public class StupActivity extends BaseActivity {
     }
 
     private void initChart() {
-
-
         mChart = (LineChart) findViewById(R.id.chart1);
-        //   mChart.setOnChartGestureListener(this);
-        // mChart.setOnChartValueSelectedListener(this);
         mChart.setDrawGridBackground(false);
-
-        // no description text
-        //  mChart.getDescription().setEnabled(false);
-
-        // enable touch gestures
         mChart.setTouchEnabled(true);
-
+        mChart.setHighlightPerDragEnabled(false);//能否拖拽高亮线(数据点与坐标的提示线)，默认是true
+        mChart.setHighlightPerTapEnabled(false);
         // enable scaling and dragging
         mChart.setDragEnabled(true);
         mChart.setScaleEnabled(true);
-        // mChart.setScaleXEnabled(true);
-        // mChart.setScaleYEnabled(true);
-
-        // if disabled, scaling can be done on x- and y-axis separately
         mChart.setPinchZoom(true);
-
-        // set an alternative background color
-        // mChart.setBackgroundColor(Color.GRAY);
-
-        // create a custom MarkerView (extend MarkerView) and specify the layout
-        // to use for it
-//        MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
-//        mv.setChartView(mChart); // For bounds control
-//        mChart.setMarker(mv); // Set the marker to the chart
 
         // x-axis limit line
         LimitLine llXAxis = new LimitLine(10f, "Index 10");
@@ -72,91 +51,51 @@ public class StupActivity extends BaseActivity {
 
         XAxis xAxis = mChart.getXAxis();
         xAxis.enableGridDashedLine(10f, 10f, 0f);
-        mChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM); // 让x轴在下面
-        //xAxis.setValueFormatter(new MyCustomXAxisValueFormatter());
-        //xAxis.addLimitLine(llXAxis); // add x-axis limit line
-
-
-        // Typeface tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+        mChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
 
         LimitLine ll1 = new LimitLine(150f, "Upper Limit");
         ll1.setLineWidth(4f);
         ll1.enableDashedLine(10f, 10f, 0f);
         ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
         ll1.setTextSize(10f);
-        //     ll1.setTypeface(tf);
 
         LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
         ll2.setLineWidth(4f);
         ll2.enableDashedLine(10f, 10f, 0f);
         ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
         ll2.setTextSize(10f);
-        //   ll2.setTypeface(tf);
 
         YAxis leftAxis = mChart.getAxisLeft();
         leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
-//        leftAxis.addLimitLine(ll1);
-//        leftAxis.addLimitLine(ll2);
-//        leftAxis.setAxisMaximum(200f);
-//        leftAxis.setAxisMinimum(-50f);
-        //leftAxis.setYOffset(20f);
         leftAxis.enableGridDashedLine(10f, 10f, 0f);
         leftAxis.setDrawZeroLine(false);
-
-        // limit lines are drawn behind data (and not on top)
         leftAxis.setDrawLimitLinesBehindData(true);
 
         mChart.getAxisRight().setEnabled(false);
         mChart.getLegend().setEnabled(false);
-        //mChart.getViewPortHandler().setMaximumScaleY(2f);
-        //mChart.getViewPortHandler().setMaximumScaleX(2f);
+        mChart.setDescription("");  //显示右下角描述
 
-        // add data
         setData(7, 100);
-
-//        mChart.setVisibleXRange(20);
-//        mChart.setVisibleYRange(20f, AxisDependency.LEFT);
-//        mChart.centerViewTo(20, 50, AxisDependency.LEFT);
-
         mChart.animateX(2500);
-        //mChart.invalidate();
+        mChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM); // 让x轴在下面
 
-        // get the legend (only possible after setting data)
-//        Legend l = mChart.getLegend();
-//
-//        // modify the legend ...
-//        l.setForm(Legend.LegendForm.LINE);
-
-        // // dont forget to refresh the drawing
-        // mChart.invalidate();
-
-        List<ILineDataSet> sets = mChart.getData()
-                .getDataSets();
-
+        List<ILineDataSet> sets = mChart.getData().getDataSets();
         for (ILineDataSet iSet : sets) {
-
             LineDataSet set = (LineDataSet) iSet;
-            set.setMode(set.getMode() == LineDataSet.Mode.CUBIC_BEZIER
-                    ? LineDataSet.Mode.LINEAR
-                    : LineDataSet.Mode.CUBIC_BEZIER);
+            set.setMode(set.getMode() == LineDataSet.Mode.CUBIC_BEZIER? LineDataSet.Mode.LINEAR: LineDataSet.Mode.CUBIC_BEZIER);
         }
-
     }
 
     private void setData(int count, float range) {
 
-        ArrayList<Entry> values = new ArrayList<Entry>();
-
+        ArrayList<Entry> values = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-
             float val = (float) (Math.random() * range) + 3;
             values.add(new Entry(i, val));
         }
 
         LineDataSet set1;
-
-        if (mChart.getData() != null &&
-                mChart.getData().getDataSetCount() > 0) {
+        if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0) {
             set1 = (LineDataSet)mChart.getData().getDataSetByIndex(0);
             set1.setValues(values);
             mChart.getData().notifyDataChanged();
@@ -164,41 +103,23 @@ public class StupActivity extends BaseActivity {
         } else {
             // create a dataset and give it a type
             set1 = new LineDataSet(values, "DataSet 1");
-
-            // set the line to be drawn like this "- - - - - -"
-//            set1.setDrawCubic(true);  //设置曲线为圆滑的线
-
-            //  set1.enableDashedLine(10f, 5f, 0f);
-            //  set1.enableDashedHighlightLine(10f, 5f, 0f);
-            //set1.setColor(R.drawable.fade_red);
-            //   set1.setCircleColor(Color.BLACK);
             set1.setLineWidth(1f);
-            //  set1.setCircleRadius(3f);
             set1.setDrawCircleHole(false);
             set1.setValueTextSize(9f);
             set1.setCubicIntensity(0.3f);
-          //  set1.setDrawFilled(true);
-
-//            set1.setFormLineWidth(1f);
-//            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
-//            set1.setFormSize(15.f);
 
             if (Utils.getSDKInt() >= 18) {
                 // fill drawable only supported on api level 18 and above
                     Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
-                   set1.setFillDrawable(drawable);
+                    set1.setFillDrawable(drawable);
             }
             else {
-                 set1.setFillColor(Color.BLACK);
+                    set1.setFillColor(Color.BLACK);
             }
 
             ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
             dataSets.add(set1); // add the datasets
-
-            // create a data object with the datasets
             LineData data = new LineData(dataSets);
-
-            // set data
             mChart.setData(data);
         }
     }
