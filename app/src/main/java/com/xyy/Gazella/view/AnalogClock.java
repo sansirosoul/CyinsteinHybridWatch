30147fd0d29538c9b78ed7eb73069aa2c342cb7c
sayid0927 <13113639501@163.com>
2016-10-17 21:06:30 +0800

    Initial commit

--- app/src/main/java/com/xyy/Gazella/view/AnalogClock.java
+++ app/src/main/java/com/xyy/Gazella/view/AnalogClock.java
@@ -74,9 +74,9 @@ public class AnalogClock extends View {
 
 
         if (mDial == null || mHourHand == null || mMinuteHand == null) {
-            mDial = r.getDrawable(R.drawable.clock_dial);
-            mHourHand = r.getDrawable(R.drawable.appwidget_clock_hour);
-            mMinuteHand = r.getDrawable(R.drawable.appwidget_clock_minute);
+            mDial = r.getDrawable(R.drawable.page12_biaopan);
+            mHourHand = r.getDrawable(R.drawable.page12_hour_selected);
+            mMinuteHand = r.getDrawable(R.drawable.page12_minute_selected);
             mSecondHand = r.getDrawable(R.drawable.appwidget_clock_second);
         }
         a.recycle();
