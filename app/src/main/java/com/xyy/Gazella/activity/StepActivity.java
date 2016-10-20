026ce50d5653444443b7ee456ca1559800365d18
sayid0927 <13113639501@163.com>
2016-10-20 18:13:12 +0800

    Initial commit

--- app/src/main/java/com/xyy/Gazella/activity/StepActivity.java
+++ app/src/main/java/com/xyy/Gazella/activity/StepActivity.java
@@ -2,8 +2,14 @@ package com.xyy.Gazella.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -11,17 +17,23 @@ import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
-import com.ysp.newband.BaseActivity;
+import com.xyy.Gazella.fragment.StepDayFragment;
+import com.xyy.Gazella.fragment.StepMonthFragment;
+import com.xyy.Gazella.fragment.StepWeekFragment;
 import com.ysp.smartwatch.R;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class StepActivity extends BaseActivity implements OnDateSelectedListener, OnMonthChangedListener {
+import static com.ysp.newband.BaseActivity.overridePendingTransitionExit;
+
+public class StepActivity extends FragmentActivity implements OnDateSelectedListener, OnMonthChangedListener {
 
     @BindView(R.id.calendarView)
     MaterialCalendarView widget;
@@ -33,25 +45,42 @@ public class StepActivity extends BaseActivity implements OnDateSelectedListener
     Button btnDate;
     @BindView(R.id.TVTitle)
     TextView TVTitle;
+    @BindView(R.id.button_day)
+    Button butDay;
+    @BindView(R.id.button_week)
+    Button butWeek;
+    @BindView(R.id.button_month)
+    Button butMonth;
+    @BindView(R.id.ll_checkdate)
+    LinearLayout llCheckDate;
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
+
+    private ArrayList<Fragment> fragmentsList;
+    private StepDayFragment stepDayFragment;
+    private StepWeekFragment stepWeekFragment;
+    private StepMonthFragment stepMonthFragment;
+    private FragmentAdapter mFragmentAdapter;
+
+
     private boolean WidgetType = false;
     private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step);
         ButterKnife.bind(this);
-        initCalendar();
         initView();
+        initCalendar();
+        InitViewPager();
     }
-
 
     private void initView() {
 
+        TVTitle.setText("计步详情");
         btnDate.setBackground(this.getResources().getDrawable(R.drawable.page17_rili));
         btnOpt.setBackground(this.getResources().getDrawable(R.drawable.page17_share));
-        TVTitle.setText("计步详情");
     }
 
     private void initCalendar() {
@@ -60,7 +89,50 @@ public class StepActivity extends BaseActivity implements OnDateSelectedListener
         widget.setOnMonthChangedListener(this);
     }
 
-    @OnClick({R.id.btnExit, R.id.btnOpt, R.id.btnDate})
+    private void InitViewPager() {
+        fragmentsList = new ArrayList<>();
+        stepDayFragment = new StepDayFragment();
+        stepWeekFragment = new StepWeekFragment();
+        stepMonthFragment = new StepMonthFragment();
+        fragmentsList.add(stepDayFragment);
+        fragmentsList.add(stepWeekFragment);
+        fragmentsList.add(stepMonthFragment);
+
+        mFragmentAdapter = new FragmentAdapter(this.getSupportFragmentManager(), fragmentsList);
+        viewpager.setAdapter(mFragmentAdapter);
+        viewpager.setCurrentItem(0);
+
+
+        viewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        setBtnBackgroundType(0);
+                        break;
+                    case 1:
+                        setBtnBackgroundType(1);
+                        break;
+                    case 2:
+                        setBtnBackgroundType(2);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+
+    @OnClick({R.id.button_day, R.id.button_week, R.id.button_month, R.id.btnExit, R.id.btnOpt, R.id.btnDate})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnExit:
@@ -74,11 +146,13 @@ public class StepActivity extends BaseActivity implements OnDateSelectedListener
 
                 if (widget.getVisibility() == View.VISIBLE) {
                     widget.setVisibility(View.GONE);
+                    llCheckDate.setVisibility(View.VISIBLE);
                     btnDate.setBackground(this.getResources().getDrawable(R.drawable.page17_rili));
                 } else {
 
                     ///初始化日历
                     widget.setVisibility(View.VISIBLE);
+                    llCheckDate.setVisibility(View.GONE);
                     widget.setBackgroundColor(this.getResources().getColor(R.color.black));
                     widget.setArrowColor(this.getResources().getColor(R.color.white));
 
@@ -90,12 +164,48 @@ public class StepActivity extends BaseActivity implements OnDateSelectedListener
                     btnDate.setBackground(this.getResources().getDrawable(R.drawable.page23_selected_rili));
                 }
                 break;
+
+            case R.id.button_day:
+                setBtnBackgroundType(0);
+                viewpager.setCurrentItem(0);
+                break;
+            case R.id.button_week:
+                setBtnBackgroundType(1);
+                viewpager.setCurrentItem(1);
+                break;
+            case R.id.button_month:
+                setBtnBackgroundType(2);
+                viewpager.setCurrentItem(2);
+                break;
         }
-
-
-
-
     }
+
+    /***
+     * 设置障碍Butnon 背景
+     *
+     * @param type 0day   1 week  2month
+     */
+
+    private void setBtnBackgroundType(int type) {
+        switch (type) {
+            case 0:
+                butDay.setBackground(this.getResources().getDrawable(R.drawable.step_leftbtn_pressed));
+                butWeek.setBackground(this.getResources().getDrawable(R.drawable.step_inbtn_normal));
+                butMonth.setBackground(this.getResources().getDrawable(R.drawable.step_rightbtn_normal));
+                break;
+            case 1:
+                butDay.setBackground(this.getResources().getDrawable(R.drawable.step_leftbtn_normal));
+                butWeek.setBackground(this.getResources().getDrawable(R.drawable.step_inbtn_pressed));
+                butMonth.setBackground(this.getResources().getDrawable(R.drawable.step_rightbtn_normal));
+                break;
+            case 2:
+                butDay.setBackground(this.getResources().getDrawable(R.drawable.step_leftbtn_normal));
+                butWeek.setBackground(this.getResources().getDrawable(R.drawable.step_inbtn_normal));
+                butMonth.setBackground(this.getResources().getDrawable(R.drawable.step_rightbtn_pressed));
+                break;
+        }
+    }
+
 
     @Override
     public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
@@ -121,5 +231,24 @@ public class StepActivity extends BaseActivity implements OnDateSelectedListener
         return FORMATTER.format(date.getDate());
     }
 
+    public class FragmentAdapter extends FragmentPagerAdapter {
+
+        List<Fragment> fragmentList = new ArrayList<>();
+
+        public FragmentAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+            super(fm);
+            this.fragmentList = fragmentList;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+    }
 
 }
---
+++ app/src/main/java/com/xyy/Gazella/fragment/StepWeekFragment.java
@@ -0,0 +1,30 @@
+package com.xyy.Gazella.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ysp.smartwatch.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Administrator on 2016/10/11.
+ */
+
+public class StepWeekFragment extends Fragment {
+    private View view;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_step, container, false);
+
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+
+
+}
