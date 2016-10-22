0251fe59b7e8434030e128968da681822ebfb300
sayid0927 <13113639501@163.com>
2016-10-22 11:20:17 +0800

    Initial commit

--- app/src/main/java/com/xyy/Gazella/activity/SleepActivity.java
+++ app/src/main/java/com/xyy/Gazella/activity/SleepActivity.java
@@ -1,15 +1,266 @@
 package com.xyy.Gazella.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import com.ysp.newband.BaseActivity;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+import com.xyy.Gazella.fragment.StepDayFragment;
+import com.xyy.Gazella.fragment.StepMonthFragment;
+import com.xyy.Gazella.fragment.StepWeekFragment;
 import com.ysp.smartwatch.R;
 
-public class SleepActivity extends BaseActivity {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.ysp.newband.BaseActivity.overridePendingTransitionExit;
+
+public class SleepActivity extends FragmentActivity implements OnDateSelectedListener, OnMonthChangedListener {
+
+    @BindView(R.id.calendarView)
+    MaterialCalendarView widget;
+    @BindView(R.id.btnExit)
+    Button btnExit;
+    @BindView(R.id.btnOpt)
+    Button btnOpt;
+    @BindView(R.id.btnDate)
+    Button btnDate;
+    @BindView(R.id.TVTitle)
+    TextView TVTitle;
+    @BindView(R.id.button_day)
+    Button butDay;
+    @BindView(R.id.button_week)
+    Button butWeek;
+    @BindView(R.id.button_month)
+    Button butMonth;
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
+    @BindView(R.id.ll_check_date)
+    LinearLayout llCheckDate;
+
+    private ArrayList<Fragment> fragmentsList;
+    private StepDayFragment stepDayFragment;
+    private StepWeekFragment stepWeekFragment;
+    private StepMonthFragment stepMonthFragment;
+    private FragmentAdapter mFragmentAdapter;
+
+
+    private boolean WidgetType = false;
+    private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
+        ButterKnife.bind(this);
+        initView();
+        initCalendar();
+        InitViewPager();
+    }
+
+    private void initView() {
+
+        TVTitle.setText("睡眠详情");
+        btnDate.setBackground(this.getResources().getDrawable(R.drawable.page17_rili));
+        btnOpt.setBackground(this.getResources().getDrawable(R.drawable.page17_share));
+    }
+
+    private void initCalendar() {
+        widget.setVisibility(View.GONE);
+        widget.setBackgroundColor(this.getResources().getColor(R.color.black));
+        widget.setArrowColor(this.getResources().getColor(R.color.white));
+        widget.setHeaderLinearColor(this.getResources().getColor(R.color.title_gray));
+        widget.setSelectionMode(MaterialCalendarView.SELECTION_MODE_SINGLE);
+        widget.setSelectionColor(this.getResources().getColor(R.color.personalize2));
+        widget.setTileHeight(90);
+        widget.setOnDateChangedListener(this);
+        widget.setOnMonthChangedListener(this);
+    }
+
+    private void InitViewPager() {
+        fragmentsList = new ArrayList<>();
+        stepDayFragment = new StepDayFragment();
+        stepWeekFragment = new StepWeekFragment();
+        stepMonthFragment = new StepMonthFragment();
+        fragmentsList.add(stepDayFragment);
+        fragmentsList.add(stepWeekFragment);
+        fragmentsList.add(stepMonthFragment);
+
+        mFragmentAdapter = new FragmentAdapter(this.getSupportFragmentManager(), fragmentsList);
+        viewpager.setAdapter(mFragmentAdapter);
+        viewpager.setCurrentItem(0);
+
+
+        viewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        setBtnBackgroundType(0);
+                        break;
+                    case 1:
+                        setBtnBackgroundType(1);
+                        break;
+                    case 2:
+                        setBtnBackgroundType(2);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+
+    @OnClick({R.id.button_day, R.id.button_week, R.id.button_month, R.id.btnExit, R.id.btnOpt, R.id.btnDate})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnExit:
+
+                SleepActivity.this.finish();
+                overridePendingTransitionExit(SleepActivity.this);
+                break;
+            case R.id.btnOpt:
+                break;
+            case R.id.btnDate:
+
+                if (widget.getVisibility() == View.VISIBLE) {
+                    widget.setVisibility(View.GONE);
+                    llCheckDate.setVisibility(View.VISIBLE);
+                    btnDate.setBackground(this.getResources().getDrawable(R.drawable.page17_rili));
+
+                    if(!stepDayFragment.getLlDateVisible())
+                        stepDayFragment.setLlDateVisible(View.VISIBLE);
+                    if(!stepWeekFragment.getLlDateVisible())
+                        stepWeekFragment.setLlDateVisible(View.VISIBLE);
+                    if(!stepMonthFragment.getLlDateVisible())
+                        stepMonthFragment.setLlDateVisible(View.VISIBLE);
+
+                } else {
+
+                    ///初始化日历
+                    widget.setVisibility(View.VISIBLE);
+                    widget.state().edit().setCalendarDisplayMode(CalendarMode.MONTHS).commit();
+                    btnDate.setBackground(this.getResources().getDrawable(R.drawable.page23_selected_rili));
+                    llCheckDate.setVisibility(View.GONE);
+
+                    if(stepDayFragment.getLlDateVisible())
+                        stepDayFragment.setLlDateVisible(View.GONE);
+                    if(stepWeekFragment.getLlDateVisible())
+                        stepWeekFragment.setLlDateVisible(View.GONE);
+                    if(stepMonthFragment.getLlDateVisible())
+                        stepMonthFragment.setLlDateVisible(View.GONE);
+                }
+                break;
+
+            case R.id.button_day:
+                setBtnBackgroundType(0);
+                viewpager.setCurrentItem(0);
+                break;
+            case R.id.button_week:
+                setBtnBackgroundType(1);
+                viewpager.setCurrentItem(1);
+                break;
+            case R.id.button_month:
+                setBtnBackgroundType(2);
+                viewpager.setCurrentItem(2);
+                break;
+        }
+    }
+
+    /***
+     * 设置Butnon 背景
+     *
+     * @param type 0day   1 week  2month
+     */
+
+    private void setBtnBackgroundType(int type) {
+        switch (type) {
+            case 0:
+                butDay.setBackground(this.getResources().getDrawable(R.drawable.step_leftbtn_pressed));
+                butWeek.setBackground(this.getResources().getDrawable(R.drawable.step_inbtn_normal));
+                butMonth.setBackground(this.getResources().getDrawable(R.drawable.step_rightbtn_normal));
+                break;
+            case 1:
+                butDay.setBackground(this.getResources().getDrawable(R.drawable.step_leftbtn_normal));
+                butWeek.setBackground(this.getResources().getDrawable(R.drawable.step_inbtn_pressed));
+                butMonth.setBackground(this.getResources().getDrawable(R.drawable.step_rightbtn_normal));
+                break;
+            case 2:
+                butDay.setBackground(this.getResources().getDrawable(R.drawable.step_leftbtn_normal));
+                butWeek.setBackground(this.getResources().getDrawable(R.drawable.step_inbtn_normal));
+                butMonth.setBackground(this.getResources().getDrawable(R.drawable.step_rightbtn_pressed));
+                break;
+        }
+    }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+        // TVTitle.setText(getSelectedDatesString());
+
+        if (WidgetType) {
+            widget.state().edit().setCalendarDisplayMode(CalendarMode.WEEKS)
+                    .commit();
+            WidgetType = false;
+        }
+    }
+
+    @Override
+    public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+        // TVTitle.setText(FORMATTER.format(date.getDate()));
+    }
+
+    private String getSelectedDatesString() {
+        CalendarDay date = widget.getSelectedDate();
+        if (date == null) {
+            return "";
+        }
+        return FORMATTER.format(date.getDate());
+    }
+
+    public class FragmentAdapter extends FragmentPagerAdapter {
+
+        List<Fragment> fragmentList = new ArrayList<>();
+
+        public FragmentAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+            super(fm);
+            this.fragmentList = fragmentList;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
     }
 }
--- app/src/main/java/com/xyy/Gazella/fragment/StepDayFragment.java
+++ app/src/main/java/com/xyy/Gazella/fragment/StepDayFragment.java
@@ -46,20 +46,22 @@ public class StepDayFragment extends Fragment {
         mChart.setPinchZoom(false);
         mChart.setDrawBarShadow(false);
         mChart.setDrawGridBackground(false);
-        mChart.setBackgroundColor(Color.rgb(55, 55, 55));
+//        mChart.setBackgroundColor(Color.rgb(55, 55, 55));
         XAxis xAxis = mChart.getXAxis();
         //   xAxis.setStartAtZero(true);
         //  xAxis.setSpaceBetweenLabels();
-
+//        xAxis.resetLabelsToSkip();
+        mChart.refreshDrawableState();
         xAxis.setAvoidFirstLastClipping(true);
         xAxis.setTextColor(Color.rgb(255, 255, 255));
         xAxis.setAxisLineColor(Color.rgb(255, 255, 255));
+        xAxis.setGridLineWidth(1);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setLabelCount(12);
         // xAxis.setGranularity(50f);
 //        xAxis.setXEntrySpace(10f);
-        xAxis.setAxisLineWidth(10f);
-//        xAxis.setGridLineWidth(1);
+        xAxis.setAxisLineWidth(1f);
+        xAxis.setGridLineWidth(1);
         xAxis.setDrawGridLines(false);
 
         mChart.getAxisLeft().setTextColor(Color.rgb(255, 255, 255));
@@ -142,7 +144,10 @@ public class StepDayFragment extends Fragment {
             set1.setColor(Color.rgb(255, 255, 255));
             set1.setDrawValues(false);
             set1.setBarBorderWidth(10f);
-            set1.setBarBorderColor(Color.rgb(55, 55, 55));
+
+//            set1.setBarBorderColor(Color.parseColor("#00FFFFFF"));
+//            set1.setBarBorderColor(Color.alpha(0));
+            set1.setBarBorderColor(Color.rgb(32, 32, 32));
             // set1.setColors(new int[]{Color.rgb(55, 55, 55)});
             ArrayList<IBarDataSet> dataSets = new ArrayList<>();
             dataSets.add(set1);
