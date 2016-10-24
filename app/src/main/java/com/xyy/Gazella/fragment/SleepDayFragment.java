f1b8f4ad41c33d057bb48ee311d863d60b16a910
sayid0927 <13113639501@163.com>
2016-10-24 18:23:25 +0800

    Initial commit

--- app/src/main/java/com/xyy/Gazella/fragment/SleepDayFragment.java
+++ app/src/main/java/com/xyy/Gazella/fragment/SleepDayFragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
@@ -14,6 +16,7 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.xyy.Gazella.utils.SomeUtills;
 import com.xyy.Gazella.view.CreateColor;
 import com.ysp.smartwatch.R;
 
@@ -21,6 +24,7 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2016/10/11.
@@ -31,6 +35,12 @@ public class SleepDayFragment extends Fragment {
     BarChart mChart;
     @BindView(R.id.ll_date)
     LinearLayout llDate;
+    @BindView(R.id.tv_date)
+    TextView tvDate;
+    @BindView(R.id.iv_left)
+    ImageView ivLeft;
+    @BindView(R.id.iv_right)
+    ImageView ivRight;
     private View view;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -39,8 +49,11 @@ public class SleepDayFragment extends Fragment {
 
         ButterKnife.bind(this, view);
         initChart();
+        tvDate.setText(new SomeUtills().getDate(0));
+
         return view;
     }
+
 
     private void initChart() {
         mChart.setDescription("");
@@ -91,7 +104,7 @@ public class SleepDayFragment extends Fragment {
         } else {
             set1 = new BarDataSet(yVals1, "");
             set1.setColors(CreateColor.MATERIAL_COLORS);
-             set1.setDrawValues(false);
+            set1.setDrawValues(false);
             ArrayList<IBarDataSet> dataSets = new ArrayList<>();
             dataSets.add(set1);
 
@@ -105,17 +118,34 @@ public class SleepDayFragment extends Fragment {
     }
 
     /***
-     *     设置日期栏是否显示
+     * 设置日期栏是否显示
+     *
      * @param visible
      */
 
-    public  void setLlDateVisible(int visible){
+    public void setLlDateVisible(int visible) {
         llDate.setVisibility(visible);
     }
-    public  boolean getLlDateVisible(){
-        if(llDate.getVisibility()==View.VISIBLE&&llDate!=null)
+
+    public boolean getLlDateVisible() {
+        if (llDate.getVisibility() == View.VISIBLE && llDate != null)
             return true;
         else
-        return  false;
+            return false;
+    }
+
+    public void setTvDateValue(String date) {
+        tvDate.setText(date);
+    }
+
+    @OnClick({R.id.iv_left, R.id.iv_right})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_left:
+
+                break;
+            case R.id.iv_right:
+                break;
+        }
     }
 }
