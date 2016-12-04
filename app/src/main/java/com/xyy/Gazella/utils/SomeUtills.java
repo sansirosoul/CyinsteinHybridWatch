7e9cf752a1132e951762ff7bccbd7bb0362c940e
sayid0927 <13113639501@163.com>
2016-12-04 21:23:08 +0800

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/xyy/Gazella/utils/SomeUtills.java
+++ app/src/main/java/com/xyy/Gazella/utils/SomeUtills.java
@@ -213,48 +213,38 @@ public class SomeUtills {
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
+//        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+//        oks.setTitle("标题");
+//        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+//         oks.setTitleUrl("http://www.cyinstein.com");
+//        // text是分享文本，所有平台都需要这个字段
+//        oks.setText("分享文本");
+//        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+//        oks.setImagePath(Environment.getExternalStorageDirectory() + "/" + "userImage.png");//确保SDcard下面存在此张图片
+//        // url仅在微信（包括好友和朋友圈）中使用
+//        oks.setUrl("http://www.cyinstein.com");
+//        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+//        oks.setComment("评论文本");
+//        // site是分享此内容的网站名称，仅在QQ空间使用
+//        oks.setSite(activity.getString(R.string.app_name));
+//        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+//        oks.setSiteUrl("http://www.cyinstein.com");
+//        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
 
-        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
-        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
 
-        oks.setTitle("标题");
-        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        // oks.setTitleUrl("http://www.cyinstein.com");
+        oks.setTitleUrl("http://www.cyinstein.com");
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("分享文本");
+        oks.setText(activity.getString(R.string.app_name));
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImagePath(Environment.getExternalStorageDirectory() + "/" + "userImage.png");//确保SDcard下面存在此张图片
+        oks.setImagePath(Environment.getExternalStorageDirectory() + "/" + "userImage.png");
         // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://www.cyinstein.com");
+        //oks.setUrl("http://www.ibabylabs.com");
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("评论文本");
+        // oks.setComment(this.getString(R.string.share));
         // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(activity.getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
         oks.setSiteUrl("http://www.cyinstein.com");
-        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
-//        oks.setShareContentCustomizeCallback(new ShareContentCustomizeCallback() {
-//            @Override
-//            public void onShare(Platform platform, cn.sharesdk.framework.Platform.ShareParams paramsToShare) {
-//                if ("QZone".equals(platform.getName())) {
-//                    paramsToShare.setTitle(null);
-//                    paramsToShare.setTitleUrl(null);
-//                }
-//                if ("SinaWeibo".equals(platform.getName())) {
-//                    paramsToShare.setUrl(null);
-//                    paramsToShare.setText("http://www.cyinstein.com");
-//                }
-//                if ("Wechat".equals(platform.getName())) {
-//                    Bitmap imageData = BitmapFactory.decodeResource(activity.getResources(), R.drawable.ssdk_logo);
-//                    paramsToShare.setImageData(imageData);
-//                }
-//                if ("WechatMoments".equals(platform.getName())) {
-//                    Bitmap imageData = BitmapFactory.decodeResource(activity.getResources(), R.drawable.ssdk_logo);
-//                    paramsToShare.setImageData(imageData);
-//                }
-//            }
-//        });
         // 启动分享GUI
         oks.show(activity);
     }
